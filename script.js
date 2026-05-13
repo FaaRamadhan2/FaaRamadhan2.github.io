f73dacd22fc6c5f1de6de7004c11506aece8ad06
FaaRamadhan126 <fauziramadhanmc126@gmail.com>
2026-05-13 08:59:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -19067,10 +19067,6 @@ Error generating stack: ` +
           className:
             "fixed top-0 left-0 right-0 z-50 flex justify-between items-center px-5 md:px-12 py-4 backdrop-blur-sm border-b border-border/30",
           children: [
-            D.jsx("div", {
-              className: "text-primary font-bold tracking-tighter text-lg",
-              children: "Faa.",
-            }),
             D.jsxs("nav", {
               className:
                 "fixed top-0 left-0 right-0 z-50 flex justify-between items-center gap-3 px-4 md:px-12 py-4 backdrop-blur-sm border-b border-border/30",
